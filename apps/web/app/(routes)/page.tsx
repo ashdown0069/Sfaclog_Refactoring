a249338a7eb93cb431f39ea2de49dd47143aae74
jaeha kim <kimjh0069@naver.com>
2024-07-06 22:43:25 +0900

    add db test route

--- apps/web/app/(routes)/page.tsx
+++ apps/web/app/(routes)/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PopularLogs } from './components/PopularLogs';
 import Footer from '@/components/Footer/Footer';
 import { RecommendedLogs } from './components/RecommendedLogs';
-import { CarouselWrapper } from '@/components/Carousel/CarouselWrapper';
+// import { CarouselWrapper } from '@/components/Carousel/CarouselWrapper';
 export default async function page() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/logs/main`, {
     cache: 'no-cache',
@@ -10,7 +10,7 @@ export default async function page() {
   const data = await res.json();
   return (
     <>
-      <CarouselWrapper />
+      {/* <CarouselWrapper /> */}
       <main className='main__page'>
         <PopularLogs logs={data.popularLogs} />
         <RecommendedLogs logs={data.recommendLogs} />
---
+++ apps/web/app/api/test/db/route.ts
@@ -0,0 +1,21 @@
+import { connectDB } from '@/lib/db';
+import { NextResponse, type NextRequest } from 'next/server';
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+    console.log('db connected');
+    return NextResponse.json(
+      {
+        message: 'db connected',
+      },
+      { status: 200 },
+    );
+  } catch (err) {
+    return NextResponse.json(
+      {
+        message: 'can not connect to database ',
+      },
+      { status: 500 },
+    );
+  }
+}
